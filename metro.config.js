50c9ddac1d740b5a37bb6591879c560f7a8678b0
Gualberto Vannini <gualberto.vannini@coachhub.io>
2022-03-07 17:52:21 +0100

    clean up svg config

--- metro.config.js
+++ metro.config.js
@@ -1,36 +1,17 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+// configuration according to react-native-svg https://github.com/kristerkari/react-native-svg-transformer#step-3-configure-the-react-native-packager
 const {getDefaultConfig} = require('metro-config');
 
 module.exports = (async () => {
   const {
     resolver: {sourceExts, assetExts},
   } = await getDefaultConfig();
-
-  const myCustomExtensions = [...sourceExts, 'svg', 'tsx', 'ts'];
-  // process.env.RN_SRC_EXT comes from Detox Mocking Guide
-  const sourceExtensions = process.env.RN_SRC_EXT
-    ? [...process.env.RN_SRC_EXT.split(','), ...myCustomExtensions]
-    : myCustomExtensions;
-
   return {
     transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: sourceExtensions,
+      sourceExts: [...sourceExts, 'svg'],
     },
   };
 })();
